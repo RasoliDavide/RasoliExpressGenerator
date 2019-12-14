7df74b5cde6d2630b48097777a7573146ffec318
RasoliDavide <rasoli.davide@iisgalvanimi.edu.it>
2019-12-14 12:15:27 +0100

    in corso

---
+++ 3_3 - FakerJS/app.js
@@ -0,0 +1,79 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var app = express();
+var faker = require('faker');
+
+const fs = require('fs');
+
+var poeti = null;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+////////////////////////////////
+if(fs.existsSync('poeti.json'))
+{
+  poeti = require('./poeti')
+}
+else
+{
+  poeti = new Array();
+  for(let i = 0; i < 10; i++)
+  {
+    obj = {
+      firstName : faker.name.firstName(1),
+      lastName : faker.name.lastName(),
+      age : faker.random.number(90),
+      image: faker.image.people(),
+      'poesie' : [
+        faker.lorem.paragraph(),
+        faker.lorem.paragraph(),
+        faker.lorem.paragraph()
+      ]
+    }
+    console.log(obj)
+    poeti.push(obj);
+
+  }
+  console.log(poeti);
+  fs.writeFileSync("poeti.json", JSON.stringify(poeti));
+}
+console.log(poeti);
+//////////////////////////////////
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+module.exports = app;
---
+++ 3_3 - FakerJS/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const poeti = require('../poeti')
+/* GET home page. */
+var objarr = new Array();
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lista di poeti', poeti: poeti });
+});
+router.get('/details', function(req, res, next)
+{
+  const poeta = poeti[req.query.id]
+  console.log(poeta)
+  res.render('details', {poeta: poeta})
+})
+
+module.exports = router;
