b08d3e4ef941170f826f67cecbbcfd8ebeeb5e41
RasoliDavide <rasoli.davide@iisgalvanimi.edu.it>
2019-12-14 22:03:08 +0100

    Finito

--- 3_3 - FakerJS/routes/services.js
+++ 3_3 - FakerJS/routes/services.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const poeti = require('../poeti')
+const poeti = require('../poeti');
+
 router.get('/all', function(req,res,next)
 {
     res.send(JSON.stringify(poeti));
@@ -11,3 +12,13 @@ router.get('/one', function(req,res,next)
     if(poeta != "undefined")
         res.send(JSON.stringify(poeta));
 })
+router.get('/images', function(req, res, next)
+{
+    arr = new Array();
+    for(let i = 0; i < poeti.length; i++)
+    {
+        arr.push(poeti[i].image);
+    }
+    res.send(JSON.stringify(arr));
+})
+module.exports = router;
